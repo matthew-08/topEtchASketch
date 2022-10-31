2cf01e909cf5d9b34998ba457b2e1c74e7460997
Matthew <matthewjcrosby1@gmail.com>
2022-10-31 17:35:19 +0700

    added 16x16 grid layout

---
+++ java.js
@@ -0,0 +1,7 @@
+const divWrapper = document.querySelector(".divWrapper")
+
+
+for (let i = 0; i <16; i++) {
+    let innerDiv = document.createElement("div");
+    divWrapper.appendChild(innerDiv);
+}
