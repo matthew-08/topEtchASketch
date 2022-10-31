39da3717fc357c8b6a0a06ae3f57c70d098acadf
Matthew <matthewjcrosby1@gmail.com>
2022-10-31 21:24:01 +0700

    Added in menu and ability to change grid size

--- java.js
+++ java.js
@@ -1,11 +1,32 @@
-const divWrapper = document.querySelector(".divWrapper")
+const divWrapper = document.querySelector(".divWrapper");
+const inputNumber = document.querySelector("#numberInput");
 
 
-for (let i = 0; i <16; i++) {
+inputNumber.addEventListener("keydown", (e) => {
+    if (e.keyCode === 13) {
+        divWrapper.innerHTML = " "
+        const userNumber = inputNumber.value;
+        const totalSize = userNumber**2;
+        console.log(totalSize);
+        
+            for(let i = 0; i < totalSize; i++){ 
+                divWrapper.style.gridTemplateColumns = `repeat(${userNumber}, ${userNumber}fr)`
+                divWrapper.style.gridTemplateRows = `repeat(${userNumber}, ${userNumber}fr)`
+                const innerDiv = document.createElement("div");
+                innerDiv.classList.add("innerDiv")
+                divWrapper.appendChild(innerDiv);
+                innerDiv.addEventListener('mouseover', () => {
+                    innerDiv.style.backgroundColor = "black";
+                })  
+            }
+    }
+
+})
+for (let i = 0; i <256; i++) {
     const innerDiv = document.createElement("div");
-    innerDiv.classList.add("innerDiv")
+   innerDiv.classList.add("innerDiv")
     divWrapper.appendChild(innerDiv);
-    innerDiv.addEventListener('mouseover', () => {
+   innerDiv.addEventListener('mouseover', () => {
         innerDiv.style.backgroundColor = "black";
     })
 }
